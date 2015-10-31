ab8572539f99db0b5b983d89f1bece94aa020983
Azanor <lhb@sun.ac.za>
2015-10-31 08:48:50 +0200

    version update

--- package-info.java
+++ package-info.java
@@ -1,4 +1,4 @@
-@API(owner = "Thaumcraft", apiVersion = "5.0.0.1", provides = "Thaumcraft|API")
+@API(owner = "Thaumcraft", apiVersion = "5.0.0.2", provides = "Thaumcraft|API")
 package thaumcraft.api;
 
 import net.minecraftforge.fml.common.API;
