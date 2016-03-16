70c45a7eb3177332440e50104023b75d987a94ec
Azanor <azanor1@gmail.com>
2016-03-16 12:02:01 +0200

    missed this

--- package-info.java
+++ package-info.java
@@ -1,4 +1,4 @@
-@API(owner = "Thaumcraft", apiVersion = "5.2.2.0", provides = "Thaumcraft|API")
+@API(owner = "Thaumcraft", apiVersion = "5.2.3.0", provides = "Thaumcraft|API")
 package thaumcraft.api;
 
 import net.minecraftforge.fml.common.API;
